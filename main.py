bff158d4cade4abc4540cfc080fbec843910f308
learnwithdemos <32341021+learnwithdemos@users.noreply.github.com>
2017-10-07 20:52:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return 'Hello World!, I Python application is running on Azure Web App'
 
 if __name__ == '__main__':
   app.run()
