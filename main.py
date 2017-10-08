61e2ba5e7426461aad9af1cb900265a519de37be
learnwithdemos <32341021+learnwithdemos@users.noreply.github.com>
2017-10-08 17:44:28 +0530

    Update main.py
    Changed to "Hello World!"

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello Readers! I am a sample Python application running on Azure Web Apps'
+  return 'Hello Worls! I am a sample Python application running on Azure Web Apps'
 
 if __name__ == '__main__':
   app.run()
